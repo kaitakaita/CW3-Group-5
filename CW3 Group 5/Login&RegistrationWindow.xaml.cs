3474eacc5e06d9a5492c1849961c4577f0e38731
HELLO <HELLO@DESKTOP-MBMQV7C>
2025-07-12 10:11:14 +0800

    Add project files.

---
+++ CW3 Group 5/Login&RegistrationWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CW3_Group_5
+{
+    /// <summary>
+    /// Interaction logic for Login_RegistrationWindow.xaml
+    /// </summary>
+    public partial class Login_RegistrationWindow : Window
+    {
+        public Login_RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void EmailTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EmailPlaceholder.Visibility = string.IsNullOrWhiteSpace(EmailTextBox.Text)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordPlaceholder.Visibility = string.IsNullOrWhiteSpace(PasswordBox.Password)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+    }
+}
---
+++ CW3 Group 5/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+
+namespace CW3_Group_5
+{
+    public partial class MainWindow : Window
+    {
+        private readonly List<string> imagePaths = new List<string>
+        {
+            "/Images/HotelImg1.jpg",
+            "/Images/HotelImg2.jpg",
+            "/Images/HotelImg3.jpg"
+        };
+
+        private int currentImageIndex = 0;
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartSlideshow();
+        }
+
+        private void StartSlideshow()
+        {
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(3)
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            ShowImage(currentImageIndex);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            currentImageIndex = (currentImageIndex + 1) % imagePaths.Count;
+            ShowImage(currentImageIndex);
+        }
+
+        private void ShowImage(int index)
+        {
+            Uri uri = new Uri(imagePaths[index], UriKind.Relative);
+            SlideshowImage.Source = new BitmapImage(uri);
+        }
+
+        private void BookNow_Click(object sender, RoutedEventArgs e)
+        {
+            Login_RegistrationWindow loginWindow = new Login_RegistrationWindow();
+            loginWindow.Show();
+            this.Close(); // Optional
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
